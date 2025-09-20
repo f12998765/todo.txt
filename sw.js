b1f900a265f6b31bc1222d350165d2547c034b87
XIZERO <f12998765@yeah.net>
2025-09-20 20:59:45 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'todo-txt-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  console.log('Service Worker 安装中...');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('缓存文件中...');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // 如果缓存中有，直接返回
+        if (response) {
+          return response;
+        }
+        
+        // 否则从网络获取
+        return fetch(event.request).catch(function() {
+          // 网络失败时，对于HTML请求返回离线页面
+          if (event.request.destination === 'document') {
+            return caches.match('./dd_field_styles.html');
+          }
+        });
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker 激活中...');
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            console.log('删除旧缓存:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
